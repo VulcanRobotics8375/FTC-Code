6c876d5e91e5614c1cbe8f15f8939a54496ec196
Nico Paoli <nicobonik.awesome@gmail.com>
2019-09-21 12:48:34 -0700

    sourcetree test

--- Team8375/src/main/java/org/firstinspires/ftc/team8375/TeleOp/TankDrive.java
+++ Team8375/src/main/java/org/firstinspires/ftc/team8375/TeleOp/TankDrive.java
@@ -53,6 +53,8 @@ public class TankDrive extends OpMode {
         robot.stop();
     }
 
+
+    //test
     public void telemetryDrivetrainPos() {
         telemetry.addData("front Left", robot.drivetrain.getPositionFl());
         telemetry.addData("front Right", robot.drivetrain.getPositionFr());
