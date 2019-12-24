ee6b1bf31a445eca39164fee1b0c9c8a86a18f82
Nico Paoli <nicobonik.awesome@gmail.com>
2019-12-24 13:35:51 -0800

    update copyright

--- Team8375/src/main/java/org/firstinspires/ftc/team8375/Autonomous/VulcanPipeline.java
+++ Team8375/src/main/java/org/firstinspires/ftc/team8375/Autonomous/VulcanPipeline.java
@@ -1,5 +1,9 @@
 /*
- * Copyright (c) 2019 Vulcan Robotics FTC Team 8375. All Rights Reserved.
+ * Copyright (c) 2019 Vulcan Robotics FTC Team 8375. Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package org.firstinspires.ftc.team8375.Autonomous;
@@ -176,7 +180,6 @@ public abstract class VulcanPipeline extends LinearOpMode {
         robot.autoArm.setFlipPos(135);
         robot.autoArm.setClawPos(170);
         robot.autoArm.setLiftTime(1, 1600);
-        robot.autoArm.setLiftPower(0);
         robot.autoArm.setClawPos(90);
         robot.autoArm.setLiftTime(-1, 1600);
         robot.autoArm.setFlipPos(45);
@@ -192,7 +195,7 @@ public abstract class VulcanPipeline extends LinearOpMode {
     public void retractAutoArm() {
         robot.autoArm.setLiftTime(-1, 1600);
         robot.autoArm.setFlipPos(45);
-        robot.autoArm.setClawPos(70);
+        robot.autoArm.setClawPos(90);
     }
 
 
