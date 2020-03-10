71c432a76ffe4312828a44c5e1b982269771c010
nicobonik <nicobonik.awesome@gmail.com>
2020-03-10 16:24:27 -0700

    finished field centric mecanum

---
+++ Team8375/src/main/java/org/firstinspires/ftc/team8375/Autonomous/VulcanPipeline.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.team8375.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public abstract class VulcanPipeline extends LinearOpMode {
+    @Override
+    public abstract void runOpMode();
+
+
+
+
+}
