c401ce867ad6f8f7c7cff907f1508f69b15e9bec
Dylan H <hansen_dylan@comcast.net>
2020-03-29 11:57:57 -0700

    Playing around with code
    how can does math libraries

---
+++ Team8375/src/main/java/org/firstinspires/ftc/team8375/Examples/dylanLimitCode.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2020 Vulcan Robotics FTC Team 8375. Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.firstinspires.ftc.team8375.Examples;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Disabled
+@TeleOp(name = "dylanMotorLimit", group = "example")
+public class dylanLimitCode extends OpMode {
+    private DcMotor lift;
+    private Servo claw;
+    private double inputPower, lowLimit;
+    private double highLimit = 10000;
+    private double slowdownDistance = 500;
+    private double highSlowDownPoint = highLimit - slowdownDistance;
+    private double lowSlowDownPoint = lowLimit + slowdownDistance;
+    private double liftPos;
+    private double slowdownRange = lowSlowDownPoint - lowLimit;
+    private double distanceFromLimit;
+    private void slowdown(double liftPos, double slowdownRange, double slowdownPoint, double limit){
+        
+    }
+
+    public void create(){
+        lift = hwMap.DcMotor.get("lift");
+        claw = hwMap.servo.get("claw");
+        //hwMap isn't working, I'm pretending it is
+    }
+
+    @Override
+    public void init() {
+        //initialization code
+        lift.setDirection(DcMotorSimple.Direction.FORWARD);
+        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        create();
+    }
+
+    @Override
+    public void init_loop() {
+        //optional
+    }
+
+    @Override
+    public void start() {
+        //runs once when the start button is pressed
+    }
+
+    @Override
+    public void loop() {
+        inputPower = gamepad2.left_stick_y;
+        liftPos = lift.getCurrentPosition();
+
+        if(liftPos > lowSlowDownPoint && liftPos < highSlowDownPoint) {
+            lift.setPower(inputPower);
+        }
+        if(liftPos < lowSlowDownPoint && liftPos > lowLimit){
+
+        }
+
+    }
+}
